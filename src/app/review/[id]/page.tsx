caab939752dd9359f8dd0d11f40d267bbcc75bf7
likeri29 <s.pochtaruk29@gmail.com>
2024-12-24 10:33:26 +0100

    fix: update review

--- src/app/review/[id]/page.tsx
+++ src/app/review/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useParams, useRouter } from "next/navigation";
 import { ReviewForm } from "@/components";
-import { createReview } from "@/services/reviewService";
+import { updateReview } from "@/services/reviewService";
 import { toast } from "react-toastify";
 import { ReviewFormValues } from "@/types";
 
@@ -9,29 +9,29 @@ const EditReviewPage = () => {
   const router = useRouter();
   const { id } = useParams();
 
-  const handleCreateReview = async (data: ReviewFormValues) => {
-    try {
-      await createReview(data);
-      toast.success("Review added successfully!");
-      router.push("/");
-    } catch (error) {
-      console.error(error);
-      toast.error("Failed to create review");
-    }
-  };
-
   if (!id) {
     toast.error("Review with this ID does not exist");
     router.push("/");
     return;
   }
 
+  const handleUpdateReview = async (data: ReviewFormValues) => {
+    try {
+      await updateReview(+id, data);
+      toast.success("Review updated successfully!");
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to update review");
+    }
+  };
+
   return (
     <div className="p-4 max-w-6xl mx-auto">
       <h1 className="text-3xl font-semibold text-gray-800 text-center mb-6">
         Edit Review
       </h1>
-      <ReviewForm onSubmit={handleCreateReview} reviewId={+id} />
+      <ReviewForm onSubmit={handleUpdateReview} reviewId={+id} />
     </div>
   );
 };
